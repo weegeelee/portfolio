0346fc2804a8a9301fcc8b401e1aafbe1822ed28
Yu Chen <weegeelee@hotmail.com>
2025-05-05 09:03:15 +0200

    add new project

--- modal.js
+++ modal.js
@@ -1,13 +1,16 @@
 const configOverlayElementOne = document.querySelector('.cofig-overlay-one');
 const configOverlayElementTwo = document.querySelector('.cofig-overlay-two');
 const configOverlayElementThree = document.querySelector('.cofig-overlay-three');
+const configOverlayElementFour = document.querySelector('.cofig-overlay-four');
 const backdropElement = document.getElementById('backdrop');
 const projectOneElement = document.querySelector('.project-one');
 const projectTwoElement = document.querySelector('.project-two');
 const projectThreeElement = document.querySelector('.project-three');
+const projectFourElement = document.querySelector('.project-four');
 const cancelConfigOverlayElementOne = document.querySelector('.overlay-cancel-btn-one');
 const cancelConfigOverlayElementTwo = document.querySelector('.overlay-cancel-btn-two');
 const cancelConfigOverlayElementThree = document.querySelector('.overlay-cancel-btn-three');
+const cancelConfigOverlayElementFour = document.querySelector('.overlay-cancel-btn-four');
 
 function openPlayerOverlayOne(event) {
     configOverlayElementOne.style.display = 'block';
@@ -38,9 +41,20 @@ function closePlayerOverlayThree() {
     backdropElement.style.display = 'none';
 }
 
+function openPlayerOverlayFour(event) {
+    configOverlayElementFour.style.display = 'block';
+    backdropElement.style.display = 'block';
+}
+
+function closePlayerOverlayFour() {
+    configOverlayElementFour.style.display = 'none';
+    backdropElement.style.display = 'none';
+}
 projectOneElement.addEventListener('click', openPlayerOverlayOne);
 projectTwoElement.addEventListener('click', openPlayerOverlayTwo);
 projectThreeElement.addEventListener('click', openPlayerOverlayThree);
+projectFourElement.addEventListener('click', openPlayerOverlayFour);
 cancelConfigOverlayElementOne.addEventListener('click', closePlayerOverlayOne);
 cancelConfigOverlayElementTwo.addEventListener('click', closePlayerOverlayTwo);
 cancelConfigOverlayElementThree.addEventListener('click', closePlayerOverlayThree);
+cancelConfigOverlayElementFour.addEventListener('click', closePlayerOverlayFour);
